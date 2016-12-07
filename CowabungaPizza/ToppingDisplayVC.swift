b9e153766866ffeb756680db2583229a58581cd8
WadeSellers <Waderoni@gmail.com>
2016-12-07 09:16:56 -0600

    added NavController, 2 segmented controllers for Table organization

--- CowabungaPizza/ToppingDisplayVC.swift
+++ CowabungaPizza/ToppingDisplayVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ToppingDIsplayVC.swift
 //  CowabungaPizza
 //
 //  Created by Wade Sellers on 12/6/16.
@@ -8,18 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ToppingDisplayVC: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
 
 }
 
